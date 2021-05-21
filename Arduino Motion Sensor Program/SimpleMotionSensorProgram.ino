a032c29d4eceeb2e0bb4fa2f26d292a9e5f36d81
akkik04 <81925146+akkik04@users.noreply.github.com>
2021-05-20 22:26:54 -0400

    Software for the Motion Sensor.

---
+++ Arduino Motion Sensor Program/SimpleMotionSensorProgram.ino
@@ -0,0 +1,38 @@
+// declaring variables to store the pin numbers of each item.
+int led = 10;
+int sensor = 7;
+int sensor_value;
+int buzzer = 8;
+
+void setup() {
+
+  // declaring the sensor,led, and buzzer as inputs.
+  pinMode(sensor, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+
+  // initializing the serial monitor.
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  // tying the 'sensor_value' variable to the value read from the sensor.
+  sensor_value = digitalRead(sensor);
+
+  // printing the sensor value on the serial monitor.
+  Serial.println(sensor_value);
+
+  // if-statement to determine when the LED should flash and the buzzer should ring.
+  if (sensor_value == 1){
+    digitalWrite(led, HIGH);
+    tone(buzzer, 1000);
+    delay(200);
+  }
+  else{
+    digitalWrite(led, LOW);
+    noTone(buzzer);
+  }
+
+}
